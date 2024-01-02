1b87ffc0f07ad82646a8271239858f8212efb64f
Md. Abdur rahman <ar_cse@yahoo.com>
2024-01-03 00:30:13 +0600

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,120 +2,183 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'mar-babu', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['marahman', 'Social-Complainer', 'BUBT-photography-club', 'Child-Day-care ', 'Edulearn-laravel ',
+        'Vandely-Design', 'cv-with-html-css-', 'TestLab ', 'Racer-Ecommerce ', 'O-Cus', 'my_react_app',
+        'Created-React-FRESH-App', 'PHP-Mastering', 'e-shop', 'Javascript-basics_Rexercises', 'acv',
+        'ant_food', 'food-app', 'ant-food'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    facebook: '',
+    linkedin: 'md-abdur-rahman-368330139',
+    twitter: 'MarAbdur',
+    mastodon: '',
+    facebook: 'mar.babu1'',
     instagram: '',
     youtube: '', // example: 'pewdiepie'
     dribbble: '',
     behance: '',
     medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
-    skype: '',
+    dev: 'marbabu',
+    stackoverflow: '7699819/abdur-rahman', // example: '1/jeff-atwood'
+    skype: 'mar_babu',
     telegram: '',
-    website: 'https://arifszn.com',
-    phone: '',
-    email: 'arifulalamszn@gmail.com',
+    website: 'https://marbabu.wixsite.com/mysite',
+    phone: '+8801681195152',
+    email: 'ar_cse@yahoo.com',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'PHP',
     'Laravel',
     'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
+    'Vue.Js',
+    'Alpine.Js',
     'MySQL',
-    'PostgreSQL',
     'Git',
-    'Docker',
-    'PHPUnit',
     'CSS',
-    'Antd',
+    'LESS',
+    'SASS',
+    'Html5',
+    'Jquery',
+    'Ajax',
+    'Bootstrap',
+    'Livewire',
+    'React.js',
     'Tailwind',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'Xgenious',
+      position: 'Software Engineer',
+      from: 'October 2022',
       to: 'Present',
-      companyLink: 'https://example.com',
+      companyLink: 'https://xgenious.com',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Tappware Solutions Limited',
+      position: 'Software Engineer',
+      from: 'March 2021',
+      to: 'September 2022',
+      companyLink: 'http://www.tappware.com',
+    },
+    {
+      company: 'Luminouslabs',
+      position: 'Software Engineer',
+      from: 'October 2020',
+      to: 'February 2021',
+      companyLink: 'https://luminouslabsbd.com',
     },
   ],
   /* certifications: [
+ {
+      name: 'Mastering Vue.js',
+      body: 'Certiﬁcation from ostad.com (Hasin Hayder)',
+      year: 'December 2023',
+      link: 'https://ostad.app/share/certificate/c7693-md.-abdur-rahman'
+    },
     {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
+      name: 'SQL (Basic)',
+      body: 'Certiﬁcation from hackerrank.com',
       year: 'March 2022',
-      link: 'https://example.com'
+      link: 'https://www.hackerrank.com/certificates/9fc783f086a5'
+    },
+    {
+      name: 'The Ultimate JavaScript Mastery Series',
+      body: 'Certiﬁcation fromcodewithmosh.com',
+      year: 'February 2021',
+      link: '#'
+    },
+    {
+      name: 'Programming Foundations with JavaScript, HTML and CSS',
+      body: 'Certiﬁcation from coursera.org',
+      year: 'July 2020',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/2RPR5KUJWYHD'
+    },
+    {
+      name: 'Front-End Web UI Frameworks and Tools',
+      body: 'Certiﬁcation from coursera.org',
+      year: 'September 2020',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/FGPXJNWTEUV7'
     },
   ], */
   education: [
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
+      institution: 'Bangladesh University of Business and Technology',
+      degree: 'Bachelor of Science',
       from: '2015',
-      to: '2019',
+      to: '2019'
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
+      institution: 'Major General Mahmudul Hasan Ideal College',
+      degree: 'Higher Secondary Certificate (HSC)',
       from: '2012',
       to: '2014',
     },
+    /*{
+        institution: 'Dighulia Shahid Mizanur Rahman High School',
+        degree: 'Secondary School Certificate (SSC)',
+        from: '2006',
+        to: '2011'
+    }*/
   ],
 
   // To hide the `My Projects` section, keep it empty.
   externalProjects: [
-    {
-      title: 'Project Name',
+     {
+      title: 'Xgapiclient',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+        'This package help to update xgenious products from user dashbaord by just click.',
+      imageUrl: 'https://drive.google.com/file/d/1hBszV-_vDG8_1TPpuaKteT4nRevFgRRe/view?usp=sharing',
+      link: 'https://packagist.org/packages/xgenious/xgapiclient',
     },
     {
-      title: 'Project Name',
+      title: 'Xg License Server',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+        'License system and automatic update features the xgenious CMS',
+      imageUrl: 'https://drive.google.com/file/d/1ncpLiqCHFNgP8OAD4eLEaZe3rxsdr_D-/view?usp=sharing',
+      link: 'http://license.xgenious.com',
+    },
+    {
+      title: 'Ditopic',
+      description:
+        'Sass Based Multitenancy with Frontend pagebuilder - CMS',
+      imageUrl: 'https://drive.google.com/file/d/1MMSr6qaBUl3AeQO3i4JZdoXQ1gIUADCs/view?usp=sharing',
+      link: 'https://ditopic.store',
+    },
+    {
+      title: 'Bst Mode',
+      description:
+        'Social media site where users will upload their best photos and submit to challenges depending on the category they chose. Users will get the opportunity to “vote” on other users’ submissions, by voting, photos will gain popularity and accumulate votes so users can move up in rank(10 ranks/levels). As users move up in rank and gain votes, they will gain a variety of emblems corresponding to their rank/status. Once users reach a certain level, users will be eligible to win prizes.... and more features...!',
+      imageUrl: 'https://drive.google.com/file/d/1kH1buTrMaUO3F9iqQNYkUI7hjlyDikRz/view?usp=sharing',
+      link: 'https://www.loom.com/share/411b58d23ff941cba65db82680a8d3b1',
+    },
+    {
+      title: 'Weather App',
+      description:
+        'Vite-Vannila Weather App!',
+      imageUrl: 'https://drive.google.com/file/d/1OLa7d5Vr5idG5TtPRjkqCfgxf4bUQcj_/view?usp=sharing',
+      link: 'https://www.loom.com/share/411b58d23ff941cba65db82680a8d3b1',
     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
+    username: 'marbabu', // to hide blog section, keep it empty
+    limit: 7, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
-    id: '',
+    id: '3811149',
     snippetVersion: 6,
   },
   themeConfig: {
