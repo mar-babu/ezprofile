0d435b393ee41b0fdfe9cc2b525173d32a7f3760
Md. Abdur rahman <ar_cse@yahoo.com>
2024-08-18 23:55:07 +0600

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -3,7 +3,7 @@
 const config = {
   github: {
     username: 'mar-babu', // Your GitHub org/user name. (Required)
-    sortBy: 'stars', // stars | updated
+    sortBy: 'updated', // stars | updated
     limit: 20, // How many projects to display.
     exclude: {
       forks: true, // Forked projects will not be displayed if set to true.
